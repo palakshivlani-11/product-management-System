652f53be16085d54b366396dde4a02b42ae65d10
Palak Shivlani <palakshivlani2001@gmail.com>
2021-05-06 18:41:51 +0530

    new commits

--- pms/admin.py
+++ pms/admin.py
@@ -2,8 +2,9 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
+from import_export.admin import ImportExportModelAdmin
 
-class productAdmin(admin.ModelAdmin):
+class productAdmin(ImportExportModelAdmin):
     list_display = ('modelno','size','price')
     search_fields = ('modelno','size','water','gas','air','wpf','density','vescosity','temp','pressure','distance','price')
     list_per_page = 15
--- productsuvana/settings.py
+++ productsuvana/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'pms',
+    'import_export',
 ]
 
 MIDDLEWARE = [
@@ -155,3 +156,7 @@ MESSAGE_TAGS = {
         messages.WARNING: 'alert-warning',
         messages.ERROR: 'alert-danger',
  }
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
